d67a6326699d19fcf367b00da7d8257cbff18057
JeuriMorel <rikonikage@yahoo.com>
2023-04-26 18:27:41 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div>
+        <h2 data-weight="light">here's some light text</h2>
+        
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
